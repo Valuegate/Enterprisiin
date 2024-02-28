f695a6fc9d36bbab4d19dbf95eada4b3c1db30c7
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-02-28 06:49:38 +0100

    almost done with the notification page

--- src/components/NotificationSettings/NotificationSettings.tsx
+++ src/components/NotificationSettings/NotificationSettings.tsx
@@ -24,85 +24,25 @@ const NotificationSettings = () => {
           Choose when and how we notify you.
         </p>
 
-        <div className="w-[800px] pt-[15px]">
-          <div className="flex flex-col gap-[9px]">
-            <div className="flex justify-between">
-              <div className="flex gap-2">
-                <span className="w-[6px] h-[6px] bg-light-black-5 rounded-full mt-2"></span>
-                <div className="">
-                  <h2 className="font-semibold text-base text-light-black-5">
-                    Lorem ipsum dolor sit amet
-                  </h2>
-                  <p className="font-medium text-base text-light-black-5 w-[600px]">
-                    Lorem ipsum dolor sit amet, cons piscing elit lorem
-                    ipsumsit. Lorem ipsum dolor sit amet, cons piscing elit
-                    lorem ipsumsit.
+        <div className="w-[800px] mt-5 md:w-full">
+          <div className="flex flex-col gap-5 w-full">
+            {notifications.map((notification, i) => {
+              return (
+                <div className="flex flex-col" key={i}>
+                  <div className="flex w-full items-center justify-between">
+                    <h2 className="semi-3 text-contrast-80 md:text-[14px] md:leading-[24px]">
+                      &#8226;<span className="pl-2">{notification.header}</span>
+                    </h2>
+                    <SwitchDemo />
+                  </div>
+                  <p className="med-3 pl-3 text-contrast-base md:text-[14px] w-[600px] md:w-full md:mt-3 mt-2">
+                    {notification.content}
                   </p>
                 </div>
-              </div>
-              <div className="">
-                <SwitchDemo />
-              </div>
-            </div>
-
-            <div className="flex justify-between">
-              <div className="flex gap-2">
-                <span className="w-[6px] h-[6px] bg-light-black-5 rounded-full mt-2"></span>
-                <div className="">
-                  <h2 className="font-semibold text-base text-light-black-5">
-                    Lorem ipsum dolor sit amet
-                  </h2>
-                  <p className="font-medium text-base text-light-black-5 w-[600px]">
-                    Lorem ipsum dolor sit amet, cons piscing elit lorem
-                    ipsumsit. Lorem ipsum dolor sit amet, cons piscing elit
-                    lorem ipsumsit.
-                  </p>
-                </div>
-              </div>
-              <div className="">
-                <SwitchDemo />
-              </div>
-            </div>
-
-            <div className="flex justify-between">
-              <div className="flex gap-2">
-                <span className="w-[6px] h-[6px] bg-light-black-5 rounded-full mt-2"></span>
-                <div className="">
-                  <h2 className="font-semibold text-base text-light-black-5">
-                    Lorem ipsum dolor sit amet
-                  </h2>
-                  <p className="font-medium text-base text-light-black-5 w-[600px]">
-                    Lorem ipsum dolor sit amet, cons piscing elit lorem
-                    ipsumsit. Lorem ipsum dolor sit amet, cons piscing elit
-                    lorem ipsumsit.
-                  </p>
-                </div>
-              </div>
-              <div className="">
-                <SwitchDemo />
-              </div>
-            </div>
-
-            <div className="flex justify-between">
-              <div className="flex gap-2">
-                <span className="w-[6px] h-[6px] bg-light-black-5 rounded-full mt-2"></span>
-                <div className="">
-                  <h2 className="font-semibold text-base text-light-black-5">
-                    Lorem ipsum dolor sit amet
-                  </h2>
-                  <p className="font-medium text-base text-light-black-5 w-[600px]">
-                    Lorem ipsum dolor sit amet, cons piscing elit lorem
-                    ipsumsit. Lorem ipsum dolor sit amet, cons piscing elit
-                    lorem ipsumsit.
-                  </p>
-                </div>
-              </div>
-              <div className="">
-                <SwitchDemo />
-              </div>
-            </div>
+              );
+            })}
           </div>
-          <div className="pt-6 pb-20">
+          <div className="py-6">
             <button
               type="submit"
               className="mt-8 bg-blue-base rounded w-[138px] h-[40px] text-white"
